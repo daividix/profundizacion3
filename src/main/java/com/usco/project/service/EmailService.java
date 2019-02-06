d1deeeb0f9ceb174d085f89581bebb7ab24438cb
Daividix <david_acss@hotmail.com>
2019-02-06 17:33:53 -0500

    mailgun

--- src/main/java/com/usco/project/service/EmailService.java
+++ src/main/java/com/usco/project/service/EmailService.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
-/* import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage; */
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 import com.sendgrid.Content;
 import com.sendgrid.Email;
@@ -15,23 +15,23 @@ import com.sendgrid.Request;
 import com.sendgrid.Response;
 import com.sendgrid.SendGrid;
 
-/* import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.mail.javamail.MimeMessagePreparator; */
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.beans.factory.annotation.Value;
 
 @Service("email_service")
 public class EmailService {
 
-    /* private JavaMailSender mailSender;
+    private JavaMailSender mailSender;
 
     @Autowired
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
-    } */
+    }
 
     @Value("${serverinfo.url}")
     String url;
@@ -40,7 +40,7 @@ public class EmailService {
         String htmlMsg = "<h3>Bienvenido a Buscapp</h3>" + "<p>Para activar tu cuenta da click <a href=\"" + url + link
                 + "\">aqui</a></p>" + "<br>"
                 + "<p style=\"font-style: oblique\">Si crees que es un error has caso omiso a este mensaje</p>";
-        Email emailFrom = new Email(from);
+        /* Email emailFrom = new Email(from);
         String emailSubject = subject;
         Email emailTo = new Email(to);
         Content emailContent = new Content("text/html", htmlMsg);
@@ -60,9 +60,9 @@ public class EmailService {
 
         } catch (IOException e) {
             System.out.println(e);
-        }
+        } */
 
-        /* MimeMessagePreparator messagePreparator = mimeMessage -> {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setFrom(from);
             messageHelper.setTo(to);
@@ -73,6 +73,6 @@ public class EmailService {
             mailSender.send(messagePreparator);
         } catch (MailException e) {
             System.out.println(e);
-        } */
+        }
     }
 }
