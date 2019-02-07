13a406177bc55d5f4e539ecd57c3d8a447ef18bd
Daividix <david_acss@hotmail.com>
2019-02-06 22:37:22 -0500

    email fixed

--- src/main/java/com/usco/project/service/EmailService.java
+++ src/main/java/com/usco/project/service/EmailService.java
@@ -2,61 +2,62 @@ package com.usco.project.service;
 
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+/* import java.io.IOException;
 
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage; */
 
-import com.sendgrid.Content;
+/* import com.sendgrid.Content;
 import com.sendgrid.Email;
 import com.sendgrid.Mail;
 import com.sendgrid.Method;
 import com.sendgrid.Request;
 import com.sendgrid.Response;
-import com.sendgrid.SendGrid;
+import com.sendgrid.SendGrid; */
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
-import com.sun.jersey.multipart.FormDataMultiPart;
-import com.sun.jersey.multipart.file.FileDataBodyPart;
+/* import com.sun.jersey.multipart.FormDataMultiPart;
+import com.sun.jersey.multipart.file.FileDataBodyPart; */
 
-import java.io.File;
+/* import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse; */
 import javax.ws.rs.core.MediaType;
 
-import org.springframework.beans.factory.annotation.Autowired;
+/*import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.mail.javamail.MimeMessagePreparator; */
 import org.springframework.beans.factory.annotation.Value;
 
 @Service("email_service")
 public class EmailService {
 
-    private JavaMailSender mailSender;
+    @Value("${serverinfo.url}")
+    String url;
+    /* private JavaMailSender mailSender;
 
     @Autowired
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
-    @Value("${serverinfo.url}")
-    String url;
+    
 
     public void sendActivationEmail(String from, String to, String subject, String link) throws IOException {
         String htmlMsg = "<h3>Bienvenido a Buscapp</h3>" + "<p>Para activar tu cuenta da click <a href=\"" + url + link
                 + "\">aqui</a></p>" + "<br>"
                 + "<p style=\"font-style: oblique\">Si crees que es un error has caso omiso a este mensaje</p>";
-        /* Email emailFrom = new Email(from);
+        Email emailFrom = new Email(from);
         String emailSubject = subject;
         Email emailTo = new Email(to);
         Content emailContent = new Content("text/html", htmlMsg);
@@ -76,7 +77,7 @@ public class EmailService {
 
         } catch (IOException e) {
             System.out.println(e);
-        } */
+        }
 
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
@@ -90,7 +91,7 @@ public class EmailService {
         } catch (MailException e) {
             System.out.println(e);
         }
-    }
+    } */
 
     public ClientResponse sendComplexMessage(String to, String link) {
         String htmlMsg = "<html><h3>Bienvenido a Buscapp</h3>" + "<p>Para activar tu cuenta da click <a href=\"" + url + link
