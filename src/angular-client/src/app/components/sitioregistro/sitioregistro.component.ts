be8ec8014c09a21cacacb013a569b4d620e3b1c8
Something <jairod15@hotmail.com>
2019-02-16 18:02:26 -0500

    update 3.0

---
+++ src/angular-client/src/app/components/sitioregistro/sitioregistro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sitioregistro',
+  templateUrl: './sitioregistro.component.html',
+  styleUrls: ['./sitioregistro.component.scss']
+})
+export class SitioregistroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
